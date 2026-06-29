477704032d9be5e4ca32c7aba01a8d732bc52aec
Sven Humburg <sven@humburg.net>
2026-06-29 10:21:44 +0200

    changes login

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'sql7.freesqldatabase.com',
-  user: 'sql7791714',
-  password: 'SRVWzfyZn2',
-  database: 'sql7791714',
+  host: 'db5018277516.hosting-data.io', // 'sql7.freesqldatabase.com',
+  user: 'dbu563873', // 'sql7791714',
+  password: 'sql7791714', // 'SRVWzfyZn2',
+  database: 'dbs14491693', // 'sql7791714',
   waitForConnections: true,
   connectionLimit: 10,
 });
